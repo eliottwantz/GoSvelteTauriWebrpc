e2935f3a08dc6ee83c2eb33daa838e1daf7805ee
Eliott Wantz <70651737+EliottWantz@users.noreply.github.com>
2023-06-24 17:14:52 -0400

    safe fetch method

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,7 +1,28 @@
-import { ApiService, type Fetch } from "./api.gen";
+import { ApiService, WebrpcError, type Fetch } from "./api.gen";
 
 const hostname = "http://localhost:8080";
 const fetcher: Fetch = (input, init) => {
   return fetch(input, init);
 };
 export const api = new ApiService(hostname, fetcher);
+
+async function safeFetch<T>(
+  fetchMethod: () => Promise<T>
+): Promise<Result<T, WebrpcError>> {
+  try {
+    return { data: await fetchMethod() };
+  } catch (e) {
+    if (e instanceof WebrpcError) {
+      const { message, name, cause, status, code } = e;
+      console.log(message, name, cause, status, code);
+      const err = { error: e, cause: cause ? cause : message };
+      return err;
+    } else {
+      console.error(e);
+    }
+  }
+}
+
+export type Result<T, E extends Error> = Ok<T> | Err<E>;
+export type Ok<T> = { data: T };
+export type Err<E> = { error: E; cause: string };
